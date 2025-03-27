b717c8b24999251ecc5ad2fab30ac3d81d53f9d5
mbarros46 <miguelbarrosramos46@gmail.com>
2025-03-27 08:56:28 -0300

    ajustes

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import styles from './Footer.module.css';
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2025 Luxury Motors. All rights reserved.</p>
+      <p>© 2025 West Garage. All rights reserved.</p>
       <div className={styles.footerLinks}>
         <a href="#">Privacidade</a>
         <a href="#">Termos</a>
